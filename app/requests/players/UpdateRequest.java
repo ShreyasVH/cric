7f7c8ecf25d18513f4244475870df575e805e7ad
ShreyasVH <44293779+ShreyasVH@users.noreply.github.com>
2020-10-21 21:14:59 +0530

    added dateOfBirth field for players (#28)

--- app/requests/players/UpdateRequest.java
+++ app/requests/players/UpdateRequest.java
@@ -3,6 +3,12 @@ package requests.players;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.util.StringUtils;
+import java.text.ParseException;
+import exceptions.BadRequestException;
+import enums.ErrorCode;
 
 @Getter
 @Setter
@@ -10,11 +16,22 @@ import lombok.Setter;
 public class UpdateRequest
 {
     private String name;
+    private String dateOfBirth;
     private Long countryId;
     private String image;
 
     public void validate()
     {
-
+        if(!StringUtils.isEmpty(this.dateOfBirth))
+        {
+            try
+            {
+                Date dateOfBirth = (new SimpleDateFormat("yyyy-MM-dd").parse(this.dateOfBirth));
+            }
+            catch(ParseException ex)
+            {
+                throw new BadRequestException(ErrorCode.INVALID_REQUEST.getCode(), "Invalid date of birth");
+            }
+        }
     }
 }
