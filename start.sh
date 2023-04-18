82ea995827c8ad4bb9d3c8f7a2b098151779d368
shreyasEverwell <shreyas@everwell.org>
2023-04-18 20:09:31 +0530

    updated local run script

--- start.sh
+++ start.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
-sbt -jvm-debug "8059" -Dlogger.file=conf/logger.xml -Dhttps.port="10059" "run 9059";
+
+if [[ ! $(lsof -i:3306 -t | wc -l) -gt 0 ]]; then
+	echo "Starting mysql"
+	mysqld_safe --defaults-file=/home/shreyas/.asdf/installs/mysql/8.0.31/conf/my.cnf > mysql.log 2>&1 &
+fi
+
+while [[ ! $(lsof -i:3306 -t | wc -l) -gt 0 ]];
+do
+	echo "Waiting for mysql"
+done
+
+set -o allexport;
+source .env;
+set +o allexport;
+sbt -jvm-debug "8059" -Dlogger.file=conf/logger.xml "run 9059";
